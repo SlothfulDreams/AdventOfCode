f7be912789d0375e1c29f302987a2f0f6d4a142b
SlothfulDreams <bensonzheng87901@gmail.com>
2024-12-01 20:45:45 -0500

    Refractored and added annotations

--- 2024/python/day1/main.py
+++ 2024/python/day1/main.py
@@ -1,31 +1,47 @@
 from collections import Counter
+from typing import List
 
 # Part One
 left_column: list = []
 right_column: list = []
 
 
-with open("input.txt", "r") as f:
-    for content in f:
-        columns = content.split()
-        left_column.append(int(columns[0]))
-        right_column.append(int(columns[1]))
+def read_columns(file_path: str) -> tuple[List[int], List[int]]:
+    left_column, right_column = [], []
 
-left_column = sorted(left_column)
-right_column = sorted(right_column)
+    try:
+        with open(file_path, "r") as f:
+            for content in f:
+                columns = content.split()
+                left_column.append(int(columns[0]))
+                right_column.append(int(columns[1]))
+    except Exception as error:
+        print(error)
 
-distances = list(map(lambda x, y: abs(y - x), left_column, right_column))
+    left_column.sort()
+    right_column.sort()
 
-print(sum(distances))
+    return left_column, right_column
+
+
+def calc_distance(left: List[int], right: List[int]) -> int:
+    return sum(map(lambda x, y: abs(y - x), left_column, right_column))
+
+
+left_column, right_column = read_columns("input.txt")
+
+print(calc_distance(left_column, right_column))
+
 
 # Part Two
 
-similarity_score = 0
+def calc_similarity_score(column: List[int]) -> int:
+    similarity_score = 0
+    count_right_column = Counter(right_column)
+    for val in column:
+        if val in count_right_column.keys():
+            similarity_score += count_right_column[val] * val
+    return similarity_score
 
-count_right_column = Counter(right_column)
 
-for val in left_column:
-    if val in count_right_column.keys():
-        similarity_score += count_right_column[val] * val
-
-print(similarity_score)
+print(calc_similarity_score(left_column))
