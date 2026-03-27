9b1558157e5a1400e443e8a4e647b3d979246747
SlothfulDreams <85036693+SlothfulDreams@users.noreply.github.com>
2026-03-27 01:31:50 -0400

    code simplification

--- 2015/python/day03.py
+++ 2015/python/day03.py
@@ -43,63 +43,31 @@ def part1(input: str) -> int:
 
 def part2(input: str) -> int:
     """
-    robot verison of santa, alternate turns with actual santa
+    Santa and robo santa alternate position
     """
-    global_house_posn = {(0, 0)}
-    
-    total_houses = 1
-    
-    robo_north_south = 0
-    robo_east_west = 0
-    
-    santa_north_south = 0
-    santa_east_west = 0
-    
+    global_house_posn: set[tuple[int, int]] = {(0, 0)}
+    positions = {"santa": [0, 0], "robo": [0, 0]}
     isSantaTurn = True
-    
-    for direction in input:
-        if isSantaTurn:
-            match direction:
-                case "^":
-                    santa_north_south += 1
-                case "v":
-                    santa_north_south -= 1
-                    
-                case "<":
-                    santa_east_west -= 1
-                case ">":
-                    santa_east_west += 1
-            
-            coords = (santa_north_south, santa_east_west)
-            
-            if coords not in global_house_posn:
-                total_houses += 1
-                global_house_posn.add(coords)
-        else:
-            match direction:
-                case "^":
-                    robo_north_south += 1
-                case "v":
-                    robo_north_south -= 1
-                    
-                case "<":
-                    robo_east_west -= 1
-                case ">":
-                    robo_east_west += 1
-            
-            coords = (robo_north_south, robo_east_west)
-            
-            if coords not in global_house_posn:
-                total_houses += 1
-                global_house_posn.add(coords)
-        isSantaTurn = not isSantaTurn
-                
-    return total_houses
-            
-print(part2("^v^v^v^v^v"))
 
+    for direction in input:
+        pos = positions["santa"] if isSantaTurn else positions["robo"]
+
+        match direction:
+            case "^": pos[0] += 1
+            case "v": pos[0] -= 1
+            case "<": pos[1] -= 1
+            case ">": pos[1] += 1
+
+        coords = (pos[0], pos[1])
+        if coords not in global_house_posn:
+            global_house_posn.add(coords)
+
+        isSantaTurn = not isSantaTurn
+
+    return len(global_house_posn)  
+            
 
 with open("input.txt", "r") as f:
     lines = f.readline()
-    print(part1(lines)) # 2081
-    print(part2(lines)) # 2341
+    print(part1(lines))
+    print(part2(lines))
